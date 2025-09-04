4eb47846b0974245e5e72b6b3d54ada70fa4d797
HHHAAAANNNNN <magmafarhan74@Gmail.com>
2025-09-04 11:34:12 +0700

    add assets and initiate login page

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('Home');
+});
+
+// default route
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('Home');
+Route::get('/login', function () {
+    return view('login');
 });
