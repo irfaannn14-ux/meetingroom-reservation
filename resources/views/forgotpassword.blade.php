7a422f7b2b343c04917817ef8f86d81ca00cecd3
intannuraini2722-art <intannuraini2722@gmail.com>
2025-09-05 19:15:43 +0700

    add forgot password

--- resources/views/forgotpassword.blade.php
+++ resources/views/forgotpassword.blade.php
@@ -6,7 +6,103 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Montserrat, sans-serif;
+            background-color: #C9DFF2;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .forgot-card {
+            background: #fff;
+            padding: 40px;
+            border-radius: 6px;
+            box-shadow: 0px 2px 8px rgba(0,0,0,0.2);
+            width: 300px;
+            text-align: center;
+        }
+
+        .forgot-card h2{
+            color: #1D64F2;
+            margin-bottom: 5px;
+        }
+        .forgot-card p{
+            font-size: 12px;
+            color: #1D64F2;
+            margin-bottom: 30px;
+        }
+        .forgot-card label{
+            display: block;
+            text-align: left;
+            margin-bottom: 5px;
+            font-size: 12px;
+            color: #565656;
+        }
+
+        .form-group input {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-size: 14px;
+        box-sizing: border-box;
+        margin-bottom: 20px;
+        }
+        .forgot-password {
+        display: flex;
+        text-align: center;
+        font-size: 10px;
+        margin-top: -15px;
+        margin-bottom: 30px;
+        gap: 6px;
+        color: #555;
+        text-decoration: none;
+        cursor: pointer;
+        }
+        .forgot-password.arrow{
+        margin-right: 6px;
+        text-decoration: none;
+        }
+
+        .forgot-card button {
+        width: 100%;
+        background-color: #1D64F2;
+        color: white;
+        border: none;
+        padding: 8px;
+        font-size: 16px;
+        font-weight: bold;
+        border-radius: 4px;
+        cursor: pointer;
+        margin-top: 25px;
+        }
+        .forgot-password:hover {
+        text-decoration: underline;
+        }
+        .forgot-card button:hover {
+        background-color: #010D26;
+        }
+
+    </style>
 </head>
 <body>
-    <h1>ini forgot password</h1>
+    <div class="forgot-card">
+        <a href="" class="forgot-password" >
+            <span class="arrow">&lt;</span>
+            <span>Forgot Password</span>
+        </a>
+        <h2>Forgot Password?</h2>
+        <p>Enter your email addres</p>
+        <form>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" placeholder="Enter your email" required>
+            </div>
+        <button type="submit">Send</button>
+        </form>
+    </div>
 </body>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,100 +1,108 @@
-<?php
-?>
-<style>
-    body {
-        margin: 0;
-        padding: 0;
-        font-family: Montserrat, sans-serif;
-        background-color: #C9DFF2;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
-    .login-card {
-        background: #fff;
-        padding: 40px;
-        border-radius: 6px;
-        box-shadow: 0px 2px 8px rgba(0,0,0,0.2);
-        width: 300px;
-        text-align: center;
-    }
-    .logo{
-        width: 50px;
-        height: auto;
+<?php ?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Montserrat, sans-serif;
+            background-color: #C9DFF2;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .login-card {
+            background: #fff;
+            padding: 40px;
+            border-radius: 6px;
+            box-shadow: 0px 2px 8px rgba(0,0,0,0.2);
+            width: 300px;
+            text-align: center;
+        }
+        .logo{
+            width: 50px;
+            height: auto;
+            margin-bottom: 15px;
+        }
+        .login-card h2{
+            color: 1D64F2;
+            margin-bottom: 5px;
+        }
+        .login-card p{
+            font-size: 12px;
+            color: #1D64F2;
+            margin-bottom: 20px;
+        }
+        .login-card label{
+            display: block;
+            text-align: left;
+            margin-bottom: 5px;
+            font-size: 12px;
+            color: #565656;
+        }
+        .login-card input[type="email"],
+        .login-card input[type="password"] {
+        width: 100%;
+        padding: 10px;
         margin-bottom: 15px;
-    }
-    .login-card h2{
-        color: 1D64F2;
-        margin-bottom: 5px;
-    }
-    .login-card p{
+        border: 1px solid #ccc;
+        border-radius: 4px;
         font-size: 12px;
-        color: #1D64F2;
-        margin-bottom: 20px;
-    }
-    .login-card label{
+        }
+        .forgot-password {
         display: block;
-        text-align: left;
-        margin-bottom: 5px;
+        text-align: right;
         font-size: 12px;
-        color: #565656;
-    }
-    .login-card input[type="email"],
-    .login-card input[type="password"] {
-      width: 100%;
-      padding: 10px;
-      margin-bottom: 15px;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 12px;
-    }
-     .forgot-password {
-      display: block;
-      text-align: right;
-      font-size: 12px;
-      margin-top: -10px;
-      margin-bottom: 20px;
-      color: #1D64F2;
-      text-decoration: none;
-    }
+        margin-top: -10px;
+        margin-bottom: 20px;
+        color: #1D64F2;
+        text-decoration: none;
+        }
 
-    .forgot-password:hover {
-      text-decoration: underline;
-    }
+        .forgot-password:hover {
+        text-decoration: underline;
+        }
 
-    .login-card button {
-      width: 100%;
-      background-color: #1D64F2;
-      color: white;
-      border: none;
-      padding: 12px;
-      font-size: 16px;
-      font-weight: bold;
-      border-radius: 4px;
-      cursor: pointer;
-    }
+        .login-card button {
+        width: 100%;
+        background-color: #1D64F2;
+        color: white;
+        border: none;
+        padding: 12px;
+        font-size: 16px;
+        font-weight: bold;
+        border-radius: 4px;
+        cursor: pointer;
+        }
 
-    .login-card button:hover {
-      background-color: #010D26;
-    }
+        .login-card button:hover {
+        background-color: #010D26;
+        }
 
-</style>
-<div class="login-card">
-    <img src="{{ asset('images/logoipsum.png') }}" alt="Logo" class="logo">
-    <h2>Login to Your Account</h2>
-    <p>Enter your email & password to login</p>
+    </style>
+</head>
+<body>
+    <div class="login-card">
+        <img src="{{ asset('images/logoipsum.png') }}" alt="Logo" class="logo">
+        <h2>Login to Your Account</h2>
+        <p>Enter your email & password to login</p>
 
-    <form>
-      <label for="email">Email</label>
-      <input type="email" id="email" placeholder="Enter your email" required>
+        <form>
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Enter your email" required>
 
-      <label for="password">Password</label>
-      <input type="password" id="password" placeholder="Enter your password" required>
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Enter your password" required>
 
-      <a href="#" class="forgot-password">Forgot Password?</a>
+        <a href="#" class="forgot-password">Forgot Password?</a>
 
-      <button type="submit">Login</button>
-    </form>
-</div>
+        <button type="submit">Login</button>
+        </form>
+    </div>
+</body>
 </php>
